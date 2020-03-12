654236594fd97ff6e0631c490f0e0848dff8d7e1
renehorstmann <r.horsty@gmail.com>
2020-03-12 11:16:35 +0100

    logc update

--- include/bindingc/log.h
+++ include/bindingc/log.h
@@ -11,6 +11,10 @@
 #ifndef LOGC_LOG_H
 #define LOGC_LOG_H
 
+#ifndef LOGC_LEADING_TEXT
+#define LOGC_LEADING_TEXT ""
+#endif
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -24,20 +28,19 @@ enum bc_log_level {
     LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_WTF, LOG_NUM_LEVELS
 };
 
-#define bc_log_trace(...) bc_log_base_(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
-#define bc_log_debug(...) bc_log_base_(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define bc_log_info(...)  bc_log_base_(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define bc_log_warn(...)  bc_log_base_(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
-#define bc_log_error(...) bc_log_base_(LOG_ERROR,   __FILE__, __LINE__, __VA_ARGS__)
-#define bc_log_wtf(...)   bc_log_base_(LOG_WTF,   __FILE__, __LINE__, __VA_ARGS__)
+#define bc_log_trace(...) log_base_(LOG_TRACE, LOGC_LEADING_TEXT, __FILE__, __LINE__, __VA_ARGS__)
+#define bc_log_debug(...) log_base_(LOG_DEBUG, LOGC_LEADING_TEXT, __FILE__, __LINE__, __VA_ARGS__)
+#define bc_log_info(...)  log_base_(LOG_INFO,  LOGC_LEADING_TEXT, __FILE__, __LINE__, __VA_ARGS__)
+#define bc_log_warn(...)  log_base_(LOG_WARN,  LOGC_LEADING_TEXT, __FILE__, __LINE__, __VA_ARGS__)
+#define bc_log_error(...) log_base_(LOG_ERROR, LOGC_LEADING_TEXT, __FILE__, __LINE__, __VA_ARGS__)
+#define bc_log_wtf(...)   log_base_(LOG_WTF,   LOGC_LEADING_TEXT, __FILE__, __LINE__, __VA_ARGS__)
 
 void bc_log_set_log_file(FILE *file);
 void bc_log_set_min_level(enum bc_log_level level);
 void bc_log_set_quiet(bool set);
-void bc_log_set_leading_text(const char *pointer_to_text);
 void bc_log_set_locking_function(bc_log_lock_function fun);
 void bc_log_set_locking_function_user_data(void *user_data);
 
-void bc_log_base_(enum bc_log_level level, const char *file, int line, const char *format, ...);
+void log_base_(enum bc_log_level level, const char *leading_text, const char *file, int line, const char *format, ...);
 
 #endif //LOGC_LOG_H
--- src/log.c
+++ src/log.c
@@ -35,7 +35,6 @@ static struct {
     FILE *log_file;
     enum bc_log_level level;
     bool quiet;
-    const char *leading_text;
     bc_log_lock_function lock_function;
     void *used_data;
 } local;
@@ -78,10 +77,6 @@ void bc_log_set_quiet(bool set) {
     local.quiet = set;
 }
 
-void bc_log_set_leading_text(const char *pointer_to_text) {
-    local.leading_text = pointer_to_text;
-}
-
 void bc_log_set_locking_function(bc_log_lock_function fun) {
     local.lock_function = fun;
 }
@@ -91,7 +86,7 @@ void bc_log_set_locking_function_user_data(void *user_data) {
 }
 
 
-void bc_log_base_(enum bc_log_level level, const char *file, int line, const char *format, ...) {
+void log_base_(enum bc_log_level level, const char *leading_text, const char *file, int line, const char *format, ...) {
     if (level < local.level) {
         return;
     }
@@ -109,19 +104,11 @@ void bc_log_base_(enum bc_log_level level, const char *file, int line, const cha
         char buf[16];
         buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_DO_NOT_USE_COLOR
-        if(local.leading_text)
-            fprintf(stderr, "%s %s %-5s %s:%d: ",
-                    local.leading_text, buf, level_names[level], file, line);
-        else
-            fprintf(stderr, "%s %-5s %s:%d: ",
-                    buf, level_names[level], file, line);
+        fprintf(stderr, "%s %-5s %s:%d: %s",
+                buf, level_names[level], file, line, leading_text);
 #else
-        if (local.leading_text)
-            fprintf(stderr, "%s %s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
-                    local.leading_text, buf, level_colors[level], level_names[level], file, line);
-        else
-            fprintf(stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
-                    buf, level_colors[level], level_names[level], file, line);
+        fprintf(stderr, LOGC_LEADING_TEXT "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m %s",
+                buf, level_colors[level], level_names[level], file, line, leading_text);
 #endif
         va_start(args, format);
         vfprintf(stderr, format, args);
@@ -135,12 +122,8 @@ void bc_log_base_(enum bc_log_level level, const char *file, int line, const cha
         va_list args;
         char buf[32];
         buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-        if (local.leading_text)
-            fprintf(local.log_file, "%s %s %-5s %s:%d: ",
-                    local.leading_text, buf, level_names[level], file, line);
-        else
-            fprintf(local.log_file, "%s %-5s %s:%d: ",
-                    buf, level_names[level], file, line);
+        fprintf(local.log_file, LOGC_LEADING_TEXT "%s %-5s %s:%d: %s",
+                buf, level_names[level], file, line, leading_text);
         va_start(args, format);
         vfprintf(local.log_file, format, args);
         va_end(args);
